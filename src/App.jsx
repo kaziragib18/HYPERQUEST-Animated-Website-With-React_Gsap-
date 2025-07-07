e8f0bb760cefe9a1ece96ba9a91c3aab0cc6009c
Kazi Md. Ragib <58323531+kaziragib18@users.noreply.github.com>
2025-07-08 05:23:55 +0600

    global css added, Started hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "./components/hero";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main className="relative flex min-h-screen w-screen overflow-hidden">
+      <Hero />
+    </main>
+  );
+};
 
-export default App
+export default App;
